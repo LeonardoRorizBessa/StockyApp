7be6d7ce4dbce8cd8f3ccd98522a0deba30e340d
LeonardoRorizBessa <leonardo.rorizbessa@gmail.com>
2026-03-05 20:01:57 -0300

    feat: tabs

--- src/app/(protected)/_layout.tsx
+++ src/app/(protected)/_layout.tsx
@@ -1,4 +1,5 @@
-import { Redirect, Stack } from 'expo-router'
+import { Redirect, Stack, Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
 
 import { useAuth } from '@/hooks/useAuth'
 import { ActivityIndicator } from 'react-native'
@@ -17,8 +18,46 @@ export default function ProtectedLayout(){
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Tabs screenOptions={{
+        tabBarActiveTintColor: '#284496',
+        tabBarInactiveTintColor: '#072a63',
+        tabBarStyle: {
+          backgroundColor: '#fff',
+          borderTopWidth: 0,
+          height: 70,
+          paddingBottom: 10,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: '500',
+        }
+      }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Estoque',
+            headerShown: false,
+            tabBarIcon: ({ color }) => <Ionicons name="cube-outline" size={24} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="scanner"
+          options={{
+            title: 'Escanear',
+            headerShown: false,
+            tabBarIcon: ({ color }) => <Ionicons name="barcode-outline" size={24} color={color} />,
+          }}
+        />
+        <Tabs.Screen
+          name="perfil"
+          options={{
+            title: 'Perfil',
+            headerShown: false,
+            tabBarIcon: ({ color }) => <Ionicons name="person-outline" size={24} color={color} />,
+          }}
+        />
+      </Tabs>
+    </>
   )
 }
